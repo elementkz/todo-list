6e4876238ccf16a4192917d05764d80b4daddeea
adam roitub <adamroitub7@gmail.com>
2024-06-19 16:09:09 +0300

    initial, made basic header layout + basic theme option

---
+++ app.js
@@ -0,0 +1,26 @@
+let root = document.querySelector(':root');
+
+let themes = undefined;
+(function load_theme(){
+    fetch('themes.json')
+        .then(res => res.json())
+        .then(json => themes = json);
+})();
+
+function theme() {
+    if (typeof themes === 'undefined')
+        console.error('[App Error] Failed to load themes');
+
+
+	let current_mode = document.querySelector('input[name=\"theme\"]:checked').id;
+    if (typeof current_mode === 'undefined')
+        console.error('[App Error] Failed to load current theme');
+
+
+
+    document.documentElement.style.setProperty('--bg', themes[current_mode].bg);
+    document.documentElement.style.setProperty('--del', themes[current_mode].del);
+    document.documentElement.style.setProperty('--text', themes[current_mode].text);
+
+
+}
