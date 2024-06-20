aa3dda3cba27a7ecff41e085cb4ff5d36fdddc98
adam roitub <adamroitub7@gmail.com>
2024-06-20 09:14:01 +0300

    made adding item work with inner array

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 let themes = undefined;
-(function load_theme(){
-    fetch('themes.json')
-        .then(res => res.json())
-        .then(json => themes = json);
+(function load_theme() {
+	fetch("themes.json")
+		.then((res) => res.json())
+		.then((json) => (themes = json));
 })();
 
 function theme() {
-    if (typeof themes === 'undefined')
-        console.error('[App Error] Failed to load themes');
+	if (typeof themes === "undefined")
+		console.error("[App Error] Failed to load themes");
 
+	let current_mode = document.querySelector('input[name="theme"]:checked').id;
+	if (typeof current_mode === "undefined")
+		console.error("[App Error] Failed to load current theme");
 
-	let current_mode = document.querySelector('input[name=\"theme\"]:checked').id;
-    if (typeof current_mode === 'undefined')
-        console.error('[App Error] Failed to load current theme');
-
-
-
-    document.documentElement.style.setProperty('--bg', themes[current_mode].bg);
-    document.documentElement.style.setProperty('--del', themes[current_mode].del);
-    document.documentElement.style.setProperty('--text', themes[current_mode].text);
+	document.documentElement.style.setProperty("--bg", themes[current_mode].bg);
+	document.documentElement.style.setProperty(
+		"--del",
+		themes[current_mode].del
+	);
+	document.documentElement.style.setProperty(
+		"--text",
+		themes[current_mode].text
+	);
 }
 
-let list = {}
+function note_template(id, content, timestamp) {
+	this.id = id;
+	this.content = content;
+	this.timestamp = timestamp;
+}
+
+let list = [];
+
+function add_item() {
+	let input = document.getElementById("new-todo").value;
+	if (input != "") {
+		list.push(new note_template(1, input, Date.now()));
+		document.getElementById("new-todo").value = "";
+	}
+}
